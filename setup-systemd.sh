cc2a9d90354d8d279a60f71e3893c93f014ee232
swaqar <sadaat_waqar@yahoo.com>
2025-12-28 18:25:47 +0500

    Add timingBreakdown.js to setup script downloads
    - Include timingBreakdown.js in curl-based deployments
    - Required for Phase 2.4 response time breakdown feature

--- setup-systemd.sh
+++ setup-systemd.sh
@@ -83,6 +83,7 @@ if [ -d "$INSTALL_DIR" ]; then
         curl -sSL https://raw.githubusercontent.com/swaqar/statusbeacon-probe/main/dnsMonitoring.js -o dnsMonitoring.js
         curl -sSL https://raw.githubusercontent.com/swaqar/statusbeacon-probe/main/cookieJar.js -o cookieJar.js
         curl -sSL https://raw.githubusercontent.com/swaqar/statusbeacon-probe/main/redirectTracking.js -o redirectTracking.js
+        curl -sSL https://raw.githubusercontent.com/swaqar/statusbeacon-probe/main/timingBreakdown.js -o timingBreakdown.js
     fi
 else
     # Fresh install - clone from git
